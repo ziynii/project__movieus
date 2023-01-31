6424ba7ea3c935efe6e39af521845dd4e081b2c3
Jiyeon <ziyeon.ivi@gmail.com>
2023-01-31 16:06:54 +0900

    💄 Styled Login page for tablet+ size

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tr  from-gray-900 to-indigo-900">
+      <div className="flex flex-col items-center justify-center rounded-md bg-gray-900 bg-opacity-80 p-12">
         <div>
           <Image src="/logo.svg" alt="logo" width={256} height={72} />
         </div>
         <p className="mt-6 text-2xl font-bold">LOGIN</p>
         <form className="mt-6 flex w-full flex-col">
           <label htmlFor="email">이메일</label>
-          <div className="flex mt-2">
+          <div className="mt-2 flex">
             <input
               id="email"
               required={true}
               placeholder="이메일을 입력해주세요"
               className=" flex-grow rounded px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900"
             />
-            <button className="ml-2 h-12 w-20 rounded flex items-center justify-center bg-white text-indigo-500">
+            <button className="ml-2 flex h-12 w-20 hover:bg-indigo-50 items-center justify-center rounded bg-white text-indigo-500">
               <svg
                 className="h-6 w-6"
                 fill="none"
@@ -48,7 +48,7 @@ export default function Login() {
               className="mt-2 flex-grow rounded px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900"
             />
           </div>
-          <button className="mt-8 h-14 w-48 rounded-full bg-indigo-500">
+          <button className="mt-8 h-14 w-48 rounded-full bg-indigo-500 hover:bg-indigo-700 ">
             로그인
           </button>
         </form>
