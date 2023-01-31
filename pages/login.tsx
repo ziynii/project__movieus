8126ad11dc6b34dfd2c6dc86f10d2c7d57f6000c
Jiyeon <ziyeon.ivi@gmail.com>
2023-01-31 15:22:08 +0900

    💄 Styled Login page for mobile size

---
+++ pages/login.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <Image src="/logo.svg" alt="logo" width={256} height={72} />
+        </div>
+        <p className="mt-6 text-2xl font-bold">LOGIN</p>
+        <form className="mt-6 flex w-full flex-col">
+          <label htmlFor="email">이메일</label>
+          <div className="flex mt-2">
+            <input
+              id="email"
+              required={true}
+              placeholder="이메일을 입력해주세요"
+              className=" flex-grow rounded px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            />
+            <button className="ml-2 h-12 w-20 rounded flex items-center justify-center bg-white text-indigo-500">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </form>
+
+        <form className="mt-4 flex w-full flex-col items-center">
+          <div className="flex w-full flex-col">
+            <label htmlFor="token">인증번호</label>
+            <input
+              id="token"
+              required={true}
+              placeholder="메일로 발송된 인증번호를 입력해주세요"
+              className="mt-2 flex-grow rounded px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            />
+          </div>
+          <button className="mt-8 h-14 w-48 rounded-full bg-indigo-500">
+            로그인
+          </button>
+        </form>
+        <p className="mt-6 text-xs">
+          MOVIEUS는 이메일을 이용한 일회성 로그인을 지원합니다
+        </p>
+      </div>
+    </div>
+  );
+}
