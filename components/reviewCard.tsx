9b04a2f14c62ea031e644344534911114b52141d
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-04 03:54:38 +0900

    💄 Styled ReviewCard component for tablet+ size

--- components/reviewCard.tsx
+++ components/reviewCard.tsx
@@ -16,21 +16,43 @@ export default function ReviewCard({ showPoster }: IReviewCardProps) {
             <span className="text-xs text-gray-300">날짜</span>
           </div>
         </div>
-        <button className="rounded-md border border-indigo-500 bg-indigo-50 px-3 py-2 text-xs text-indigo-500 hover:bg-indigo-100">
-          좋아요 17
+        <button className="flex items-center rounded-md border border-indigo-500 bg-indigo-50 px-3 py-2 text-xs font-medium text-indigo-500 hover:bg-indigo-100 md:px-4 md:py-2 md:text-base">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            className="mr-1 md:mr-3 h-4 w-4 md:h-6 md:w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+            ></path>
+          </svg>{' '}
+          17456
         </button>
       </div>
 
-     <Rate rate={3}/>
+      <Rate rate={3} />
 
-      <div className={'mt-4' + (showPoster ? ' flex items-start' : '')}>
-				{showPoster && <div className='w-20 h-32 bg-gray-400 shrink-0'/>}
-				<pre className={"whitespace-pre-wrap text-sm" + (showPoster ? ' w-full ml-4' : '') }>
-					Tempor sed vulputate enim gravida. Condimentum cras at quisque duis
-					tortor a. Venenatis neque blandit nunc proin arcu mi habitant. Platea mi
-					posuere sociis sem egestas. Quam vitae feugiat egestas nec.
-				</pre>
-			</div>
+      <div className={'mt-4 md:mt-6' + (showPoster ? ' flex items-start' : '')}>
+        {showPoster && (
+          <div className="h-32 w-20 shrink-0 bg-gray-400 md:h-36 md:w-24" />
+        )}
+        <pre
+          className={
+            'whitespace-pre-wrap text-sm md:text-base' +
+            (showPoster ? ' ml-4 w-full' : '')
+          }
+        >
+          Tempor sed vulputate enim gravida. Condimentum cras at quisque duis
+          tortor a. Venenatis neque blandit nunc proin arcu mi habitant. Platea
+          mi posuere sociis sem egestas. Quam vitae feugiat egestas nec.
+        </pre>
+      </div>
     </li>
   );
 }
