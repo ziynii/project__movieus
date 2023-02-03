11d5e68117d08eb81fbd5a0970a791b471160c9b
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-04 03:53:19 +0900

    💄 Styled mypage/following page for tablet+ size

--- pages/mypage/following.tsx
+++ pages/mypage/following.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Following() {
   return (
     <MypageLayout tabValue="팔로잉">
-      <ul className="grid grid-cols-3 gap-y-4">
+      <ul className="grid md:py-4 grid-cols-3 md:grid-cols-4 gap-y-4">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, i) => (
           <UserCard key={i} />
         ))}
