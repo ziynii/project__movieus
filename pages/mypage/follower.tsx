7da5e47b8e89f652938bcaa70e814a8833ea4df4
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-03 20:07:52 +0900

    💄 Styled mypage/follower page for mobile size

---
+++ pages/mypage/follower.tsx
@@ -0,0 +1,16 @@
+import MypageLayout from '@/components/layout/mypageLayout';
+import UserCard from '@/components/userCard';
+import React from 'react';
+
+export default function Follower() {
+	return (
+		<MypageLayout tabValue='팔로워'>
+			 <ul className="grid grid-cols-3 gap-y-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, i) => (
+          <UserCard key={i} />
+        ))}
+      </ul>
+		</MypageLayout>
+	);
+}
+
