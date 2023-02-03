6cde6951caa49f595383345ac7759b74fa5ec452
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-04 03:47:39 +0900

    💄 Styled mypage/like page for tablet+ size

--- pages/mypage/like.tsx
+++ pages/mypage/like.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Like() {
   return (
     <MypageLayout tabValue="찜 한 영화">
-      <ul className="grid grid-cols-3 justify-items-center gap-y-4 md:grid-cols-4">
+      <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-6 justify-items-center">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, i) => (
           <MovieCard key={i} />
         ))}
