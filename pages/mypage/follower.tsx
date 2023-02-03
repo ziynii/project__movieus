1753cc7259198f23db3031bcdff05d18d8e54951
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-04 03:53:40 +0900

    💄 Styled mypage/follower page for tablet+ size

--- pages/mypage/follower.tsx
+++ pages/mypage/follower.tsx
@@ -3,14 +3,13 @@ import UserCard from '@/components/userCard';
 import React from 'react';
 
 export default function Follower() {
-	return (
-		<MypageLayout tabValue='팔로워'>
-			 <ul className="grid grid-cols-3 gap-y-4">
+  return (
+    <MypageLayout tabValue="팔로워">
+      <ul className="grid grid-cols-3 gap-y-4 md:grid-cols-4 md:pt-4">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, i) => (
           <UserCard key={i} />
         ))}
       </ul>
-		</MypageLayout>
-	);
+    </MypageLayout>
+  );
 }
-
