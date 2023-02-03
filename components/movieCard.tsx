b30e8c1b12df7978a97c94cc7a8074565643d586
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-03 19:51:59 +0900

    💄 Change width and hieght for responsive

--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function MovieCard() {
   return (
-    <li className="w-32 shrink-0">
-      <div className="relative">
-        <div className="h-48 w-full bg-slate-200" />
+    <li className="w-full cursor-pointer">
+      <div className="relative w-full">
+        <div className="w-full bg-slate-200 pb-[150%]" />
         <span className="absolute bottom-2 right-1 rounded-lg bg-white px-2 text-xs text-gray-900">
           9.8
         </span>
