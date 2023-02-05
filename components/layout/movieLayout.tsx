2914e86fd1bf93dc1a5cd9f6364e6db9cb939f39
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-06 03:49:56 +0900

    💄 Styled MovieLayout component for desktop size

--- components/layout/movieLayout.tsx
+++ components/layout/movieLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ActorCard from '../actorCard';
 import TabList from '../tabList';
 import Layout from './layout';
 
@@ -17,22 +18,37 @@ export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
   return (
     <Layout>
       <div className="relative">
-        <div className="h-80 w-full bg-gray-200" />
-        <div className="absolute -bottom-6 left-1/2 flex w-4/5 -translate-x-1/2 items-center">
-          <div className="h-40 w-28 bg-gray-400" />
-          <div className="ml-3">
-            <h4 className="text-2xl font-bold">Title</h4>
-            <span className="mt-3 inline-block w-14 rounded-full bg-indigo-500 py-1 text-center text-xs">
-              9.2
-            </span>
+        <div className="h-80 w-full bg-gray-200 lg:h-96" />
+
+        <div className="lg:relative lg:-top-14 lg:grid lg:grid-cols-5 lg:gap-x-6 lg:px-4">
+          <div className="absolute top-44 left-1/2 flex w-4/5 -translate-x-1/2 items-center lg:relative lg:top-0 lg:left-0 lg:block lg:w-full lg:translate-x-0">
+            <div className="h-40 w-28 bg-gray-400 lg:h-72 lg:w-full" />
+            <div className="ml-3 lg:ml-0 lg:mt-4">
+              <h4 className="text-2xl font-bold lg:text-5xl">Title</h4>
+              <span className="mt-3 inline-block w-14 rounded-full bg-indigo-500 py-1 text-center text-xs">
+                9.2
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-12 md:mt-20 md:px-20 lg:col-start-2 lg:col-end-5 lg:px-0 ">
+            <div className="lg:w-3/5">
+              <TabList list={TABLIST} item={tabValue} page="movie" />
+            </div>
+            <div className="px-4 py-8 md:py-12">{children}</div>
+          </div>
+
+          <div className="lg:col-start-5 lg:mt-20">
+            <p className="text-lg font-bold md:text-lg">출연진</p>
+            <ul className="mt-6 space-y-4">
+							<ActorCard />
+							<ActorCard />
+							<ActorCard />
+							<ActorCard />
+						</ul>
           </div>
         </div>
       </div>
-
-      <div className="mt-12">
-        <TabList list={TABLIST} item={tabValue} page="movie" />
-      </div>
-      <div className="px-4 py-8">{children}</div>
     </Layout>
   );
 }
