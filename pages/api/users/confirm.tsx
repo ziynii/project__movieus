213cf00fbe699564f15dac0df29e019f04289375
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-10 00:29:25 +0900

    🚧 Implement api: token check and Login

---
+++ pages/api/users/confirm.tsx
@@ -0,0 +1,32 @@
+import client from '@/libs/server/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import { withApiSession } from '@/libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { token } = req.body;
+  const foundToken = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+
+  if (!foundToken) return res.json({ ok: false });
+  req.session.user = {
+    id: foundToken?.userId,
+  };
+  await req.session.save();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+
+  return res.json({ ok: true });
+}
+
+export default withApiSession(withHandler('POST', handler));
