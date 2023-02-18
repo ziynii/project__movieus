9353e01a122c97dbd60bd051dc406915c06db98f
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-18 13:53:50 +0900

    🚧 Change methods to array

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,19 +5,21 @@ export interface ResponseType {
   [key: string]: any;
 }
 
+type method = 'GET' | 'POST' | 'DELETE';
+
 interface WithHandlerProps {
-  method: 'GET' | 'POST' | 'DELETE';
+  methods: method[]
   fn: (req: NextApiRequest, res: NextApiResponse) => void;
   isPrivate: boolean;
 }
 
 export default function withHandler({
-  method,
+  methods,
   fn,
   isPrivate,
 }: WithHandlerProps) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== method) {
+    if (req.method && !methods.includes(req.method as any)) {
       return res.status(405).end();
     }
     if (isPrivate && !req.session.user) {
