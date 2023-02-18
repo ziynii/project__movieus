e260af1454026183f8f39bef37eca181ae5b3893
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-18 13:17:57 +0900

    🚧 Add makeDate function

--- libs/client/utils.ts
+++ libs/client/utils.ts
@@ -1,3 +1,13 @@
 export function makeImagePath(id: string, format?: string) {
   return `https://image.tmdb.org/t/p/${format ? format : 'original'}${id}`;
 }
+
+export function makeDate(dates: Date) {
+
+  const dateFormat = new Date(dates);
+  const year = dateFormat.getFullYear();
+  const month = dateFormat.getMonth() + 1;
+  const date = dateFormat.getDate();
+
+  return `${year}-${month}-${date}`;
+}
