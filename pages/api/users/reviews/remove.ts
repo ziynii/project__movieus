dd24cb5b47a640f44919bac60262e698ebdfd040
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-21 21:54:15 +0900

    🚧 Implement api: remove review

---
+++ pages/api/users/reviews/remove.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import { withApiSession } from '@/libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { reviewId } = req.body;
+
+  const removeReview = await client?.review.delete({
+    where: {
+      id: reviewId,
+    },
+  });
+
+  return res.json({
+    ok: true,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    fn: handler,
+    isPrivate: true,
+  })
+);
