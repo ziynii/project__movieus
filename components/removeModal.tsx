e34f04c201fc848c9d97eda1f8931346bd29a376
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-21 21:51:27 +0900

    🚧 Implement remove my review

---
+++ components/removeModal.tsx
@@ -0,0 +1,53 @@
+import useMutation from '@/libs/client/useMutation';
+import React, { useEffect } from 'react';
+import { KeyedMutator } from 'swr';
+
+interface IRemoveModalProps {
+  setIsOpenModal: (value: boolean) => void;
+  reviewId: number;
+  mutate: KeyedMutator<any>;
+}
+
+export default function RemoveModal({
+  setIsOpenModal,
+  reviewId,
+  mutate,
+}: IRemoveModalProps) {
+  const [remove, { data, loading }] = useMutation(`/api/users/reviews/remove`);
+
+  const handleRemove = () => {
+    if (loading) return;
+    remove({ reviewId });
+  };
+
+  useEffect(() => {
+    if (data && data.ok) {
+      mutate();
+      setIsOpenModal(false);
+    }
+  }, [data, mutate, setIsOpenModal]);
+
+  return (
+    <div className="fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-slate-700 bg-opacity-40">
+      <div className="w-3/5 max-w-modal rounded-md bg-white px-4 py-2 pt-6">
+        <p className="text-center text-sm text-gray-900">
+          정말로 삭제하시겠습니까?
+        </p>
+        <div className="mt-4 flex justify-between">
+          <button
+            onClick={handleRemove}
+            className="basis-[49%] rounded bg-indigo-500 px-4 py-2 text-xs hover:bg-indigo-700 md:px-8 md:py-4 md:text-base"
+          >
+            삭제
+          </button>
+          <button
+            onClick={() => setIsOpenModal(false)}
+            className="basis-[49%] rounded border border-indigo-500 bg-white px-4 py-2 text-xs text-indigo-500 hover:bg-indigo-100 md:px-8 md:py-4 md:text-base"
+          >
+            취소
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/reviewCard.tsx
+++ components/reviewCard.tsx
@@ -1,15 +1,42 @@
+import useMutation from '@/libs/client/useMutation';
 import { makeDate, makeImagePath } from '@/libs/client/utils';
 import { ReviewWithUser } from '@/pages/movies/[id]/reviews';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import Rate from './rate';
 
 interface IReviewCardProps {
   showPoster?: boolean;
   review: ReviewWithUser;
+  type: string;
+  setIsOpenModal?: (value: boolean) => void;
+  setReviewId?: (value: number) => void;
 }
 
-export default function ReviewCard({ showPoster, review }: IReviewCardProps) {
+export default function ReviewCard({
+  showPoster,
+  review,
+  type,
+  setIsOpenModal,
+  setReviewId,
+}: IReviewCardProps) {
+  const router = useRouter();
+  const [like, { loading }] = useMutation(
+    `/api/movies/${router.query.id}/reviews/like`
+  );
+
+  const onLikeClick = () => {
+    if (loading) return;
+    like({ reviewId: review.id });
+  };
+
+  const handleRemove = () => {
+    setIsOpenModal && setIsOpenModal(true);
+    setReviewId && setReviewId(review.id);
+  };
+
   return (
     <li className="border-b border-gray-400 py-6 last:border-0">
       <div className="flex items-center justify-between">
@@ -17,27 +44,41 @@ export default function ReviewCard({ showPoster, review }: IReviewCardProps) {
           <div className="h-14 w-14 rounded-full bg-gray-400" />
           <div className="ml-4">
             <p className="text-sm font-medium">{review?.user?.name}</p>
-            <span className="text-xs text-gray-300">{makeDate(review?.createdAt)}</span>
+            <span className="text-xs text-gray-300">
+              {makeDate(review?.createdAt)}
+            </span>
           </div>
         </div>
-        <button className="flex items-center rounded-md border border-indigo-500 bg-indigo-50 px-3 py-2 text-xs font-medium text-indigo-500 hover:bg-indigo-100 md:px-4 md:py-2 md:text-base">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            className="mr-1 h-4 w-4 md:mr-3 md:h-6 md:w-6"
+        {type === 'likes' ? (
+          <button
+            onClick={onLikeClick}
+            className="flex items-center rounded-md border border-indigo-500 bg-indigo-50 px-3 py-2 text-xs font-medium text-indigo-500 hover:bg-indigo-100 md:px-4 md:py-2 md:text-base"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-            ></path>
-          </svg>{' '}
-          17456
-        </button>
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              className="mr-1 h-4 w-4 md:mr-3 md:h-6 md:w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+              ></path>
+            </svg>{' '}
+            {review?._count?.like}
+          </button>
+        ) : (
+          <button
+            onClick={handleRemove}
+            className="flex items-center rounded-md border border-indigo-500 bg-indigo-50 px-3 py-2 text-xs font-medium text-indigo-500 hover:bg-indigo-100 md:px-4 md:py-2 md:text-base"
+          >
+            삭제하기
+          </button>
+        )}
       </div>
 
       <Rate rate={review.rate} />
@@ -45,13 +86,15 @@ export default function ReviewCard({ showPoster, review }: IReviewCardProps) {
       <div className={'mt-4 md:mt-6' + (showPoster ? ' flex items-start' : '')}>
         {showPoster && (
           <div className="relative h-32 w-20 shrink-0 md:h-36 md:w-24">
-            <Image
-              src={makeImagePath(review?.posterUrl)}
-              alt={'리뷰'}
-              fill
-              sizes="100%"
-              priority={true}
-            />
+            <Link href={`/movies/${review?.movieId}/detail`}>
+              <Image
+                src={makeImagePath(review?.posterUrl)}
+                alt={'리뷰'}
+                fill
+                sizes="100%"
+                priority={true}
+              />
+            </Link>
           </div>
         )}
         <pre
