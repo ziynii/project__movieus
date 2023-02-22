6cb0d9f4bee8d4b985db7b0a57fbd2218a6a7226
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-22 21:15:06 +0900

    🚧 Add RemoveModal when isOpenModal state is true

--- pages/mypage/reviews.tsx
+++ pages/mypage/reviews.tsx
@@ -1,19 +1,33 @@
 import MypageLayout from '@/components/layout/mypageLayout';
+import RemoveModal from '@/components/removeModal';
 import ReviewCard from '@/components/reviewCard';
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import { ReviewWithUser } from '../movies/[id]/reviews';
 
 export default function Reviews() {
-  const { data } = useSWR(`/api/users/reviews`);
+  const { data, mutate } = useSWR(`/api/users/reviews`);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [reviewId, setReviewId] = useState(0);
 
   return (
     <MypageLayout tabValue="내 리뷰">
       <ul>
         {data?.reviews?.map((review: ReviewWithUser) => (
-          <ReviewCard key={review.id} review={review} showPoster />
+          <ReviewCard
+            key={review.id}
+            review={review}
+            showPoster
+            type="remove"
+            setIsOpenModal={setIsOpenModal}
+            setReviewId={setReviewId}
+          />
         ))}
       </ul>
+
+      {isOpenModal ? (
+        <RemoveModal mutate={mutate} reviewId={reviewId} setIsOpenModal={setIsOpenModal} />
+      ) : null}
     </MypageLayout>
   );
 }
