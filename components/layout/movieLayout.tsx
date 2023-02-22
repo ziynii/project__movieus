3759de44176c5fb0500ded586033f3f38915b4ea
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-22 21:12:23 +0900

    🚧 Add movie to my movie list

--- components/layout/movieLayout.tsx
+++ components/layout/movieLayout.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ActorCard from '../actorCard';
 import TabList from '../tabList';
 import Layout from './layout';
@@ -8,6 +8,9 @@ import { ICardMovie } from '@/pages';
 import Image from 'next/image';
 import { makeImagePath } from '@/libs/client/utils';
 import { ICast } from '@/pages/api/movies/[id]/credits';
+import useMutation from '@/libs/client/useMutation';
+import { IDetailMovie } from '@/pages/movies/[id]/detail';
+import { MovieLike } from '@prisma/client';
 
 interface IMovieLayoutProps {
   children: React.ReactNode;
@@ -16,7 +19,8 @@ interface IMovieLayoutProps {
 
 interface IDetail {
   ok: boolean;
-  movie: ICardMovie;
+  movie: IDetailMovie;
+  isLike: MovieLike;
 }
 
 export interface ISWRCastData {
@@ -27,11 +31,14 @@ export interface ISWRCastData {
 
 const TABLIST = [
   { link: 'detail', value: '영화 정보' },
-  { link: 'review', value: '리뷰' },
+  { link: 'reviews', value: '리뷰' },
   { link: 'recommend', value: '영화 추천' },
 ];
 
-export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
+export default function MovieLayout({
+  children,
+  tabValue,
+}: IMovieLayoutProps) {
   const router = useRouter();
   const { id } = router.query;
   const { data } = useSWR<IDetail>(
@@ -41,10 +48,28 @@ export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
     id ? `/api/movies/${id}/credits` : null
   );
 
+
+  const [fav, { loading }] = useMutation(`/api/movies/${router.query.id}/fav`);
+
+  const handleMovieFav = () => {
+    if (loading) return;
+    fav({ title: data?.movie?.title, posterUrl: data?.movie?.poster_path });
+    setIsLike((prev) => !prev);
+  };
+
+  const [isLike, setIsLike] = useState(false);
+
+  useEffect(() => {
+    if (data?.isLike) {
+      setIsLike(true);
+    }
+  }, [data?.isLike]);
+
   return (
     <Layout>
       <div className="relative">
         <div className="relative h-80 w-full lg:h-96">
+          <div className="absolute top-0 left-0 z-10 h-full w-full bg-gradient-to-b from-transparent to-gray-900" />
           <Image
             src={makeImagePath(data?.movie?.backdrop_path!)}
             alt={'영화 배경 화면'}
@@ -58,7 +83,7 @@ export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
         </div>
 
         <div className="lg:relative lg:-top-14 lg:grid lg:grid-cols-5 lg:gap-x-6 lg:px-4">
-          <div className="absolute top-44 left-1/2 flex w-4/5 -translate-x-1/2 items-center lg:relative lg:top-0 lg:left-0 lg:block lg:w-full lg:translate-x-0">
+          <div className="absolute top-44 left-1/2 z-20 flex w-4/5 -translate-x-1/2 items-center lg:relative lg:top-0 lg:left-0 lg:block lg:w-full lg:translate-x-0">
             <div className="relative h-40 w-28 lg:h-72 lg:w-full">
               <Image
                 src={makeImagePath(data?.movie?.poster_path!)}
@@ -70,12 +95,31 @@ export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
               />
             </div>
             <div className="ml-3 lg:ml-0 lg:mt-4">
-              <h4 className="text-2xl font-bold lg:text-5xl">
+              <h4 className="text-2xl font-bold lg:text-3xl">
                 {data?.movie?.title}
               </h4>
-              <span className="mt-3 inline-block w-14 rounded-full bg-indigo-500 py-1 text-center text-xs">
-                9.2
-              </span>
+
+              <button
+                onClick={handleMovieFav}
+                className="mt-3 flex rounded-full bg-indigo-700 px-4 py-1 text-center text-xs"
+              >
+                <svg
+                  fill={isLike ? 'currentColor' : 'none'}
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  className="mr-1 h-4 w-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                  ></path>
+                </svg>
+                찜하기
+              </button>
             </div>
           </div>
 
@@ -89,7 +133,9 @@ export default function MovieLayout({ children, tabValue }: IMovieLayoutProps) {
           <div className="hidden lg:col-start-5 lg:mt-20 lg:block">
             <p className="text-lg font-bold md:text-lg">출연진</p>
             <ul className="mt-6 space-y-4">
-            {credits?.casts?.map((cast) =>   <ActorCard key={cast.id} cast={cast}/>)}
+              {credits?.casts?.map((cast) => (
+                <ActorCard key={cast.id} cast={cast} />
+              ))}
             </ul>
           </div>
         </div>
