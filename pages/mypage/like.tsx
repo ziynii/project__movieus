3d7240683c233c92c265e5f769de22b5aa6d0c4e
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-22 21:17:56 +0900

    🚧 Fetch data

--- pages/mypage/like.tsx
+++ pages/mypage/like.tsx
@@ -1,13 +1,29 @@
 import MypageLayout from '@/components/layout/mypageLayout';
 import MovieCard from '@/components/movieCard';
 import React from 'react';
+import useSWR from 'swr';
+
+interface IMovieLike {
+  movieId: number;
+  title: string;
+  posterUrl: string;
+}
 
 export default function Like() {
+  const { data } = useSWR(`/api/users/fav`);
+
   return (
     <MypageLayout tabValue="찜 한 영화">
-      <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-6 justify-items-center">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, i) => (
-          <MovieCard key={i} />
+      <ul className="grid grid-cols-3 justify-items-center gap-x-4 gap-y-6 md:grid-cols-4 lg:grid-cols-5">
+        {data?.myFavMovies.map((movie: IMovieLike) => (
+          <MovieCard
+            key={movie.movieId}
+            movie={{
+              id: movie.movieId,
+              title: movie.title,
+              poster_path: movie.posterUrl,
+            }}
+          />
         ))}
       </ul>
     </MypageLayout>
