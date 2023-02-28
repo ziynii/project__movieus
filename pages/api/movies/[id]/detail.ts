6bb10ada1d330144de499954ae8ec94959ee0277
Jiyeon <ziyeon.ivi@gmail.com>
2023-02-28 12:20:28 +0900

    🚧 Add isLike data

--- pages/api/movies/[id]/detail.ts
+++ pages/api/movies/[id]/detail.ts
@@ -6,14 +6,26 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
   const movie = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=ko-KR`
   ).then((res) => res.json());
 
+  const isLike = await client?.movieLike.findFirst({
+    where: {
+      movieId: Number(id),
+      userId: user?.id,
+    },
+  });
+
   res.json({
     ok: true,
     movie,
+    isLike,
   });
 }
 
