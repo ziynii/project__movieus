dd4eddbdcc45651c43925c59ab4775ba58a4a9cd
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-02 00:28:35 +0900

    🚧 Fetch data

--- components/userCard.tsx
+++ components/userCard.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { openUserModalState, userIdState } from '@/recoil/states';
 
-export default function UserCard() {
+interface IUserCardProps {
+  user: { id: number; avatar: string | null; name: string };
+}
+
+export default function UserCard({ user }: IUserCardProps) {
+  const setIsOpenUserModal = useSetRecoilState(openUserModalState);
+  const setUserId = useSetRecoilState(userIdState);
+
+  const handleModal = () => {
+    setIsOpenUserModal(true);
+    setUserId(user?.id);
+  };
+
   return (
-    <li className="w-full px-4 py-2 text-center cursor-pointer">
+    <li
+      onClick={handleModal}
+      className="w-full cursor-pointer px-4 py-2 text-center"
+    >
       <div className="aspect-square w-full rounded-full bg-gray-400" />
-      <div className="mt-4">
-        <p className="text-base font-medium">name</p>
-        <span className="text-xs">소개글</span>
-      </div>
+      <p className="mt-4 text-base font-medium">{user?.name}</p>
     </li>
   );
 }
