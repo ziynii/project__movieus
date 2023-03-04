359a529490e22f4e9503dafb3ac1395d1458ac88
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-04 21:51:02 +0900

    🚧 Add update user data when post request

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -9,29 +9,70 @@ async function handler(
 ) {
   const { user } = req.session;
 
-  const profile = await client.user.findUnique({
-    where: {
-      id: user?.id,
-    },
-    include: {
-      _count: {
-        select: {
-          reviews: true,
-          follower: true,
-          following: true,
+  if (req.method === 'GET') {
+    const profile = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+      include: {
+        _count: {
+          select: {
+            reviews: true,
+            follower: true,
+            following: true,
+          },
         },
       },
-    },
-  });
-  res.json({
-    ok: true,
-    profile,
-  });
+    });
+    res.json({
+      ok: true,
+      profile,
+    });
+  }
+
+  if (req.method === 'POST') {
+    const { name, about } = req.body;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+
+    if (name !== currentUser?.name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+      res.json({
+        ok: true,
+      });
+    }
+
+    if (about !== currentUser?.about) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          about,
+        },
+      });
+      res.json({
+        ok: true,
+      });
+    }
+
+    res.json({ ok: true });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     fn: handler,
     isPrivate: true,
   })
