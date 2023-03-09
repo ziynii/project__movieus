0ff6f03cccbc18b6acbb3b44d896237956cf4bdd
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-09 15:25:31 +0900

    🚧 Implement ProfileImage component

---
+++ components/profileImage.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface IProfileImage {
+  avatarId: string;
+}
+
+export default function ProfileImage({ avatarId }: IProfileImage) {
+  return (
+    <Image
+      src={`https://imagedelivery.net/XdQEHVCtHrxdO7Tk-lWLsw/${avatarId}/public`}
+      alt={'프로필 이미지'}
+			fill
+			className="h-full w-full object-cover rounded-full bg-slate-500"
+    />
+  );
+}
