a699a3a48a65e7cd6cd454851d62fb8a36c0cc35
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-12 18:46:22 +0900

    🚧 Receive id as props and add it to url

--- components/tabList.tsx
+++ components/tabList.tsx
@@ -10,9 +10,10 @@ interface ITabListProps {
   list: IList[];
   item: string;
   page: string;
+  id: number;
 }
 
-export default function TabList({ list, item, page }: ITabListProps) {
+export default function TabList({ list, item, page, id }: ITabListProps) {
   return (
     <div>
       <ul className="flex justify-between ">
@@ -23,12 +24,12 @@ export default function TabList({ list, item, page }: ITabListProps) {
                 (item === tab.value
                   ? 'border-indigo-500 font-bold'
                   : 'border-gray-900') +
-                ' h-14 w-full cursor-pointer border-b-2 text-sm md:text-lg text-indigo-500'
+                ' h-14 w-full cursor-pointer border-b-2 text-sm text-indigo-500 md:text-lg'
               }
               key={i}
             >
               <Link
-                href={`/${page}/${tab.link}`}
+                href={`/${page}/${id}/${tab.link}`}
                 className="flex h-full w-full items-center justify-center"
               >
                 {tab.value}
