89b589451768d27b6f51ae7ef1ce89c6c7754c3e
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-23 02:42:03 +0900

    ⚡️ Change render method to SSR

--- pages/movies/[id]/detail.tsx
+++ pages/movies/[id]/detail.tsx
@@ -1,8 +1,9 @@
 import MovieLayout, { ICastResponse } from '@/components/layout/movieLayout';
-import { MovieLike } from '@prisma/client';
+import { ICast } from '@/pages/api/movies/[id]/credits';
+import { NextPage, GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react';
-import useSWR from 'swr';
+import useSWR, { SWRConfig, unstable_serialize } from 'swr';
 
 interface IGenre {
   id: number;
@@ -35,7 +36,7 @@ interface IVideoResponse {
   error?: string;
 }
 
-export default function Detail() {
+function Detail() {
   const router = useRouter();
   const { id } = router.query;
   const { data } = useSWR<IDetailResponse>(
@@ -44,7 +45,7 @@ export default function Detail() {
   const { data: video } = useSWR<IVideoResponse>(
     id ? `/api/movies/${id}/video` : null
   );
-  const videoId = video?.foundVideo[0]?.key;
+  const videoId = video?.ok ? video?.foundVideo[0]?.key : null;
 
   const { data: credits } = useSWR<ICastResponse>(
     id ? `/api/movies/${id}/credits` : null
@@ -55,12 +56,15 @@ export default function Detail() {
       <div>
         <p className="text-lg font-bold md:text-lg ">줄거리</p>
         <pre className="mt-4 whitespace-pre-wrap text-sm md:h-48 md:text-base">
-          {data?.movie?.overview}
+          {data?.movie?.overview === ''
+            ? '제공되는 줄거리가 없습니다.'
+            : data?.movie?.overview}
         </pre>
       </div>
 
       <div className="mt-12">
         <p className="text-lg font-bold md:text-lg ">영화 정보</p>
+
         <dl className="mt-4">
           <div className="flex items-center border-b border-gray-400 py-4">
             <dt className="basis-1/5 text-sm font-bold md:text-base">감독</dt>
@@ -94,6 +98,7 @@ export default function Detail() {
               {data?.movie?.release_date}
             </dd>
           </div>
+
           <div className="flex items-center border-b border-gray-400 py-4">
             <dt className="basis-1/5 text-sm font-bold md:text-base">런타임</dt>
             <dd className="basis-4/5 text-xs md:text-sm">
@@ -106,14 +111,71 @@ export default function Detail() {
       <div className="mt-12">
         <p className="text-lg font-bold md:text-lg ">트레일러</p>
         <div className="mt-4 aspect-video w-full">
-          {video?.ok === false && <div>{video?.error}</div>}
-          <iframe
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-          ></iframe>
+          {video?.ok ? (
+            <iframe
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+            ></iframe>
+          ) : (
+            <div>{video?.error}</div>
+          )}
         </div>
       </div>
     </MovieLayout>
   );
 }
+
+const Page: NextPage<{
+  movie: IDetailMovie;
+  id: string;
+  director: ICast;
+  casts: ICast[];
+}> = ({ movie, id, director, casts }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          [unstable_serialize(['api', 'movies', id, 'detail'])]: {
+            ok: true,
+            movie,
+          },
+          [unstable_serialize(['api', 'movies', id, 'credits'])]: {
+            ok: true,
+            director,
+            casts,
+          },
+        },
+      }}
+    >
+      <Detail />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const movie = await fetch(
+    `https://api.themoviedb.org/3/movie/${context?.params?.id}?api_key=${process.env.TMDB_API_KEY}&language=ko-KR`
+  ).then((res) => res.json());
+
+  const allCredits = await fetch(
+    `https://api.themoviedb.org/3/movie/${context?.params?.id}/credits?api_key=${process.env.TMDB_API_KEY}&language=ko-KR
+		`
+  ).then((res) => res.json());
+
+  const director = allCredits?.crew
+    .filter((cast: ICast) => cast.known_for_department === 'Directing')
+    .slice(0, 1);
+
+  const casts = allCredits?.cast.slice(0, 4);
+
+  return {
+    props: {
+      movie,
+      director,
+      casts,
+    },
+  };
+};
+
+export default Page;
--- pages/search/[keyword].tsx
+++ pages/search/[keyword].tsx
@@ -3,34 +3,73 @@ import MovieCard from '@/components/movieCard';
 import SearchBox from '@/components/searchBox';
 import { useRouter } from 'next/router';
 import React from 'react';
+import useSWR, { SWRConfig, unstable_serialize } from 'swr';
+import { GetServerSideProps, NextPage } from 'next';
 import { ICardMovie } from '..';
-import useSWR from 'swr';
 
 interface ISearchResponse {
   ok: boolean;
   searchResult: ICardMovie[];
 }
 
-export default function Search() {
+function Search() {
   const router = useRouter();
   const { keyword } = router.query;
-  const { data } = useSWR<ISearchResponse>(`/api/search/${keyword}`);
+  const { data } = useSWR<ISearchResponse>(
+    keyword ? `/api/search/${keyword}` : null
+  );
 
   return (
-    <Layout>
+    <Layout seoTitle={`${keyword}의 검색결과`}>
       <div className="py-8 px-4 md:px-10 lg:px-20">
         <div className="relative mx-auto w-4/5">
           <SearchBox />
         </div>
 
-        <p className="py-8">&apos;{keyword}&apos; 의 검색 결과입니다</p>
+        <p className="py-8">&apos;{keyword}&apos;의 검색 결과입니다 &#40;{data?.searchResult?.length}건&#41;</p>
 
         <ul className="grid grid-cols-3 justify-items-center gap-x-4 gap-y-6 md:grid-cols-4 lg:grid-cols-5">
           {data?.searchResult?.map((movie) => (
-            <MovieCard key={movie.id} movie={movie}/>
+            <MovieCard key={movie.id} movie={movie} />
           ))}
         </ul>
       </div>
     </Layout>
   );
 }
+
+const Page: NextPage<{ search: ICardMovie[]; keyword: string }> = ({
+  search,
+  keyword,
+}) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          [unstable_serialize(['api', 'search', keyword])]: {
+            ok: true,
+            search,
+          },
+        },
+      }}
+    >
+      <Search />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const search = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=ko-KR&query=${context?.params?.keyword}&page=1&include_adult=true`
+  )
+    .then((res) => res.json())
+    .then((data) => data.results);
+
+  return {
+    props: {
+      search: JSON.parse(JSON.stringify(search)),
+    },
+  };
+};
+
+export default Page;
